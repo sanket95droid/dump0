4830e90598575b7c922b7be00e066653a36a32b5
sanket95droid <sanketzambre1@gmail.com>
2024-01-09 20:17:57 +0530

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
 import {Button} from '@/components/ui/button'
+import { auth } from '@clerk/nextjs'
 
-export default function Home() {
+export default async function Home() {
+  const {userId} = await auth()
+  const isAuth = !!userId
   return (
     <div className="main text-red-800">
       <h1>Insight-PDF</h1>
