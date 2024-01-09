13bdf2cbccd55b72b0f0a7080269dac6aec34ce9
sanket95droid <sanketzambre1@gmail.com>
2024-01-10 00:08:13 +0530

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,57 @@
 import Image from 'next/image'
-import {Button} from '@/components/ui/button'
-import { auth } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+import {UserButton, auth} from '@clerk/nextjs'
+import {LogIn, Gem} from 'lucide-react'
+import Link from 'next/link'
 
 export default async function Home() {
   const {userId} = await auth()
   const isAuth = !!userId
   return (
-    <div className="main text-red-800">
-      <h1>Insight-PDF</h1>
-      <h3>Insight-PDF</h3>
-      <Button>Click Me</Button>
+    <div className="w-screen min-h-screen bg-[#505946]">
+      <div className="absolute top-[40%] inset-y-0 left-[5%] w-1/3">
+        <div className="flex flex-col">
+            <div className="flex">
+              <h1 className="mr-3 font-semibold text-5xl text-[#E9E9E9]">Elevate Your Document Experience with PDFInsight</h1>
+              <UserButton afterSignOutUrl='/'></UserButton>
+            </div>
+
+            <div className="flex mt-2">
+              {isAuth && (<Button>Go to Chats</Button>)}
+            </div>
+
+            {/* <p className='mt-3'>
+              Unleash the power of interactive PDFs with our innovative application. Engage, discuss, and understand your documents like never before.
+              Its not just reading, its a dynamic conversation with your content.
+            </p> */}
+
+            <div className="w-full mt-4 flex-row">
+              {isAuth ? (<h1></h1>):(
+                <Link href="/sign-in">
+                  <Button className="bg-[#0B0D09] shadow-lg">Get Started
+                    <LogIn className='ml-2 h-4 w-4 animate-bounce'/>
+                  </Button>
+                </Link>
+              )}
+              {isAuth ? (<h1></h1>):(
+                <Link href="/pricing" className="ml-3">
+                  <Button className="bg-[#0B0D09] shadow-lg">Pricing
+                    <Gem className='ml-2 h-4 w-4'/>
+                  </Button>
+                </Link> 
+              )}
+            </div>
+
+        </div>
+      </div>
+
+      <div className="absolute top-[40%] inset-y-0 right-[5%] w-1/2">
+        <div className="flex flex-col">
+          <Image src = "" className = "w-1/2 h-3/4" alt = "Application Image"/>
+
+        </div>
+      </div>
+
     </div>
   )
 }
