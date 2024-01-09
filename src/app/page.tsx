d4dface70a052e94e3275c77a90fa8b86af03ce6
sanket95droid <sanketzambre1@gmail.com>
2024-01-09 18:42:13 +0530

    added shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import {Button} from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div className="main">
+    <div className="main text-red-800">
       <h1>Insight-PDF</h1>
       <h3>Insight-PDF</h3>
+      <Button>Click Me</Button>
     </div>
   )
 }
