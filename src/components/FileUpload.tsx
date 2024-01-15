b38bb1a11ee1afc4a9f35c4ecc101175fd58cdb4
sanket95droid <sanketzambre1@gmail.com>
2024-01-15 22:29:27 +0530

    setup s3 bucket

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -4,7 +4,13 @@ import {useDropzone} from 'react-dropzone'
 import {Inbox} from 'lucide-react'
 
 const FileUpload = () => {
-    const {getRootProps, getInputProps} = useDropzone();
+    const {getRootProps, getInputProps} = useDropzone({
+      accept: {"application/pdf": [".pdf"]},
+      maxFiles: 1,
+      onDrop: (acceptedFiles) => {
+        console.log(acceptedFiles)
+      }
+    });
   return (
     <div className='p-3 bg-white rounded-lg ml-3'>
         <div {...getRootProps({className: "flex border-dashed border-2 w-40 py-2 rounded-lg cursor-pointer bg-gray-100 hover:bg-gray-200 items-center text-center flex-col"})}>
